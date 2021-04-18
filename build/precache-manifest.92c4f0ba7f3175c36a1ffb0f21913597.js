7181a0ef4838aa99763d44ff653ea414141e1ac7
ankit-github123 <ankit190900@gmail.com>
2021-04-18 22:28:14 +0530

    1st

---
+++ build/precache-manifest.92c4f0ba7f3175c36a1ffb0f21913597.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c191bcec32dda3445e410251b3eb48c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc81364fab498c7614d",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "4c5cbeadb7bc10f1595f",
+    "url": "/static/css/main.aefde451.chunk.css"
+  },
+  {
+    "revision": "9fc81364fab498c7614d",
+    "url": "/static/js/2.ae00c589.chunk.js"
+  },
+  {
+    "revision": "4c5cbeadb7bc10f1595f",
+    "url": "/static/js/main.0297d5d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac3f8f6c03910f87d66a74f36f5a9403",
+    "url": "/static/media/AnkitKumar_Resume.ac3f8f6c.pdf"
+  },
+  {
+    "revision": "d5ddb872ad505c99173aad301da1024e",
+    "url": "/static/media/card2.d5ddb872.jpg"
+  },
+  {
+    "revision": "d750073b883a190116dd0db95fa6ec0d",
+    "url": "/static/media/cover.d750073b.jpg"
+  },
+  {
+    "revision": "d1e1ca6ea2769eae814243f85f474c3f",
+    "url": "/static/media/digital.d1e1ca6e.jpg"
+  },
+  {
+    "revision": "144469bc65d3339846d615a1fc982602",
+    "url": "/static/media/foodie.144469bc.jpg"
+  },
+  {
+    "revision": "ba2a288366ca0165208a14a8ac4221cb",
+    "url": "/static/media/government.ba2a2883.jpg"
+  },
+  {
+    "revision": "4526fa2c4f378e4a66be8bd49bfc2177",
+    "url": "/static/media/happy.4526fa2c.jpg"
+  },
+  {
+    "revision": "c29ad9903ba9511080e5b2c98001d1b4",
+    "url": "/static/media/logomob.c29ad990.svg"
+  },
+  {
+    "revision": "572271b4c3b9a5fb84f5b0f079cde351",
+    "url": "/static/media/logonew.572271b4.svg"
+  },
+  {
+    "revision": "ab59a43c445b3d088358cc5d799d1b43",
+    "url": "/static/media/profile.ab59a43c.jpg"
+  },
+  {
+    "revision": "a9132cf14ad83b294090a082feb3d2e8",
+    "url": "/static/media/profile3.a9132cf1.jpg"
+  },
+  {
+    "revision": "a0cb4f3ea30205fffac0353a75fcfd00",
+    "url": "/static/media/pubg.a0cb4f3e.jpg"
+  },
+  {
+    "revision": "d1062ba101d7ee0d31a8feecc5bcb49c",
+    "url": "/static/media/random.d1062ba1.jpg"
+  },
+  {
+    "revision": "ec263ec9fb39cb4aa44a05431c4c36be",
+    "url": "/static/media/test3.ec263ec9.jpg"
+  },
+  {
+    "revision": "43a258d7bed6d1590c80ca85434254d0",
+    "url": "/static/media/tourism.43a258d7.jpg"
+  },
+  {
+    "revision": "c0911be7391b51ea14f373d7a0c09010",
+    "url": "/static/media/win.c0911be7.jpg"
+  }
+]);
